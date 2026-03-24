b42cfadb89bec9322761d030f93605f523112231
Ubuntu <itziktdk@openclaw.wz35xpdpdi0unmb04qoap2hopf.ax.internal.cloudapp.net>
2026-03-24 14:34:48 +0000

    📦 Add static export + GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  basePath: '/pickpick',
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
